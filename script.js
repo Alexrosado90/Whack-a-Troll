0f6a629893ab93669165714e29bb2acbf2c3ad79
Alex Rosado <Ace@ool-ad0200c4.dyn.optonline.net>
2018-06-07 13:42:34 -0400

    images added and successful test of JS

---
+++ script.js
@@ -0,0 +1,18 @@
+$(document).ready(function() {
+    console.log( "ready!" );
+
+
+});
+
+
+
+});
+
+//functions needed
+/*startGame()
+randomHole()
+countDown()
+reset()
+click() */
+//test functions
+
