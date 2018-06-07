d903e1938e3d77fd1991e22e07c7d8343dbc8aeb
Alex Rosado <Ace@ool-ad0200c4.dyn.optonline.net>
2018-06-07 14:37:52 -0400

    score counter added

--- script.js
+++ script.js
@@ -1,18 +1,33 @@
 $(document).ready(function() {
     console.log( "ready!" );
 
+const $troll = $('#troll');
+const $hole = $('#hole');
+let $playerName = $('.current-name');
+let $timer = $('.current-timer');
+let $score = $('.current-score');
+let $currentScore = 0
+
+
+// Score keeper
+$($score).append($currentScore);
+$troll.click(function() {
+    $currentScore++;
+    $($score).html($currentScore);
+    
+});
 
 });
 
 
 
-});
+
 
 //functions needed
 /*startGame()
 randomHole()
 countDown()
 reset()
-click() */
+ */
 //test functions
 
