1e4d587d0345e128c4ea47c00fd936c89c32f2ad
Alex Rosado <Ace@ool-ad0200c4.dyn.optonline.net>
2018-06-08 16:35:28 -0400

    gameboard is about 95% finished

--- script.js
+++ script.js
@@ -1,22 +1,33 @@
 $(document).ready(function() {
     console.log( "ready!" );
 
-const $troll = $('#troll');
-const $hole = $('#hole');
+let $troll = $('#troll');
+let $holes = $('.hole');
+let $plots = $('.plot');
 let $playerName = $('.current-name');
 let $timer = $('.current-timer');
 let $score = $('.current-score');
-let $currentScore = 0
+let $currentScore = 0;
+
+//Let the games begin!
+
+function play() {
+    randomHole()
+    setTimeout(play, (Math.random() * 700 + 200));
+}
 
 
 // Score keeper
 $($score).append($currentScore);
 $troll.click(function() {
     $currentScore++;
+    // let randomTrollIndex = Math.random();
     $($score).html($currentScore);
-    
-});
+    let randomPlot = Math.floor(Math.random() * 5) + 1;
+    $($plots[randomPlot]).append($troll)
 
+    console.log(randomPlot)
+});
 //setting the timer
 
     let countDown = 60
@@ -30,12 +41,24 @@ $troll.click(function() {
         }
 
     }, 1000);
-           
+
+    // Time to let the troll out!
+function randomHole() {
+ 
+//   while ($holes == $troll) {
+    // $holes = (Math.random() * 6) | 0;
+//   }
+  if ($plots == $troll) {
+    $('#troll').removeClass('active');
+    
+  }
+    //  $troll == $holes;
+  $('#troll').addClass('active');
+    
+}
+play()
+
 });
-
-
-
-
 
 //functions needed
 /*startGame()
