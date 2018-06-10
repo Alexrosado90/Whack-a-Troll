74c65188043c234605262a18e1e38ce258a4f26f
Alex Rosado <Ace@Macbooks-MacBook-Pro-4.local>
2018-06-10 12:10:43 -0400

    working on landing page

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ let $playerName = $('.current-name');
 let $timer = $('.current-timer');
 let $score = $('.current-score');
 let $currentScore = 0;
+let $reset = $('.restart')
 
 //Let the games begin!
 
@@ -18,15 +19,13 @@ function play() {
 
 
 // Score keeper
-$($score).append($currentScore);
-$troll.click(function() {
-    $currentScore++;
-    // let randomTrollIndex = Math.random();
-    $($score).html($currentScore);
-    let randomPlot = Math.floor(Math.random() * 5) + 1;
-    $($plots[randomPlot]).append($troll)
+    $($score).append($currentScore);
+    $troll.click(function() {
+        $currentScore++;
+        $($score).html($currentScore);
+        let randomPlot = Math.floor(Math.random() * 5) + 1;
+        $($plots[randomPlot]).append($troll)
 
-    console.log(randomPlot)
 });
 //setting the timer
 
@@ -38,26 +37,28 @@ $troll.click(function() {
         if (countDown == -1) {
             console.log("Game over!");
             clearInterval(clock)
+            alert('Game over!')
+            $reset.addClass('active');
+
         }
 
     }, 1000);
 
+
     // Time to let the troll out!
 function randomHole() {
- 
-//   while ($holes == $troll) {
-    // $holes = (Math.random() * 6) | 0;
-//   }
-  if ($plots == $troll) {
-    $('#troll').removeClass('active');
-    
-  }
-    //  $troll == $holes;
-  $('#troll').addClass('active');
-    
+   
 }
+$troll.addClass('active');
 play()
 
+function reset() {
+    ready();
+}
+
+$reset.click(function() {
+    Program.restart();
+   })
 });
 
 //functions needed
